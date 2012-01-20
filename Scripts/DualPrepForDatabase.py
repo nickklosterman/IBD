d4cbeb6086febc280736e1065cdb0663cb077dd4
Nicolae Ceausescu <nicolae@nicolae-desktop.(none)>
2012-01-20 00:01:03 -0500

    umm just improvements and data added

--- Scripts/DualPrepForDatabase.py
+++ Scripts/DualPrepForDatabase.py
@@ -1,11 +1,19 @@
 import sqlite3
 import sys #for cmd line arguments
+from datetime import date #for .weekday() function
 
-def read_file_build_database(table,file):
+def read_file_build_database(table,errortable,file,reportdayofweek):
     Query="DROP TABLE IF EXISTS "+table
     print Query
     cursor.execute(Query)
     Query="CREATE TABLE IF NOT EXISTS "+table+" (Id INTEGER PRIMARY KEY, Date TEXT, StockTicker TEXT, Rank INTEGER)"
+    print Query
+    cursor.execute(Query)
+
+    Query="DROP TABLE IF EXISTS "+errortable
+    print Query
+    cursor.execute(Query)
+    Query="CREATE TABLE IF NOT EXISTS "+errortable+" (Id INTEGER PRIMARY KEY, Date TEXT, StockTicker TEXT, Rank INTEGER)"
     print Query
     cursor.execute(Query)
 
@@ -15,27 +23,40 @@ def read_file_build_database(table,file):
         if not line.strip():# (len(line)<5):#check for blank lines
             counter=-1
             countermod=3
-            print "--",line,"--"
+ #           print "--",line,"--"
         
         if line[0]=='#':
             counter=-1
             countermod=3
-            print "Comment",line[1:]
+            print "Comment",line[1:].strip()
         if countermod == 0:
             Date=line[:-1].strip() #chop off the newline at the end
+            print Date
         elif countermod == 1:
 #        String=line[:-1].split(',')#split variable on delimiter ','
             String=line[:-1].split()#split variable on delimiter
             counter2=0
+            year=Date[0:4]
+            month=Date[5:7]
+            day=Date[8:10]
             for each in String: 
                 Ticker=String[counter2].strip().upper()
                 Rank=counter2+1
-                print Date,',',Ticker,',',Rank
-                Query="INSERT INTO "+table+" VALUES(null,"+Date+","+Ticker+","+str(Rank)+")"
-                print Query
-#                cursor.execute('INSERT INTO IBD50 VALUES(null, ?, ?, ?)', (Date,String[counter2].strip().upper(),counter2+1))
-                Front='INSERT INTO '+table
-                cursor.execute(Front+' VALUES(null, ?, ?, ?)', (Date,String[counter2].strip().upper(),counter2+1))
+                dayofweek=date(int(year),int(month),int(day)).weekday()
+                #perform a date check on teh data being entered. BC20 is published on Tuesday IBD50 on Monday except when bank holiday falls on Monday then it falls on a Tuesday
+                if reportdayofweek==dayofweek:
+                    Front='INSERT INTO '+table
+                    cursor.execute(Front+' VALUES(null, ?, ?, ?)', (Date,String[counter2].strip().upper(),counter2+1))
+                elif reportdayofweek==0 and dayofweek==1:#case of IBD50 bumped on a Tuesday
+                    Front='INSERT INTO '+table
+                    cursor.execute(Front+' VALUES(null, ?, ?, ?)', (Date,String[counter2].strip().upper(),counter2+1))
+                    Front='INSERT INTO '+errortable #enter it into the error dbase just in case
+                    cursor.execute(Front+' VALUES(null, ?, ?, ?)', (Date,String[counter2].strip().upper(),counter2+1))
+                else:
+#                if reportdayofweek!=dayofweek:
+                    print "Date doesn't fall on appropriate day of week, Entering into Error Database:",errortable
+                    Front='INSERT INTO '+errortable #enter it into the error dbase just in case
+                    cursor.execute(Front+' VALUES(null, ?, ?, ?)', (Date,String[counter2].strip().upper(),counter2+1))
                 #cursor.execute(Query) Using this method I don't think the strnigs were being quoted when they needed to
                 counter2+=1
         counter+=1
@@ -49,14 +70,19 @@ else:
     database="IBDdatabase.sqlite"
     table1="IBD50"
     file1 = open("Data/IBD50.txt")
+    errortable1="IBD50Error"
+    reportDayOfWeek1=0#monday
     table2="BC20"
     file2 = open("Data/BC20.txt")
+    errortable2="BC20Error"
+    reportDayOfWeek2=1#tuesday
 
 connection=sqlite3.connect(database)
 cursor=connection.cursor()
 #from http://greeennotebook.com/2010/06/how-to-use-sqlite3-from-python-introductory-tutorial/
-read_file_build_database(table1,file1)
-read_file_build_database(table2,file2)
+read_file_build_database(table1,errortable1,file1,reportDayOfWeek1)
+read_file_build_database(table2,errortable2,file2,reportDayOfWeek2)
+
 
 
 
