d11dad9e25f6ce434bc47870261c975eb49fb218
nick klosterman <nick_klosterman@yahoo.com>
2013-10-08 22:55:16 -0400

    gnuplot test

--- Scripts/GetAllDateTickerStockData.sh
+++ Scripts/GetAllDateTickerStockData.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python GetStockDataForAllDates.py3 -i BC20 -o StockData -d IBDdatabase.sqlite
-python GetStockDataForAllDates.py3 -i IBD50 -o StockData -d IBDdatabase.sqlite
-python GetStockDataForAllDates.py3 -i IBD8585 -o StockData -d IBDdatabase.sqlite
-python GetStockDataForAllDates.py3 -i Top200Composite -o StockData -d IBDdatabase.sqlite
+python Scripts/GetStockDataForAllDates.py3 -i BC20 -o StockData -d IBDdatabase.sqlite
+python Scripts/GetStockDataForAllDates.py3 -i IBD50 -o StockData -d IBDdatabase.sqlite
+python Scripts/GetStockDataForAllDates.py3 -i IBD8585 -o StockData -d IBDdatabase.sqlite
+python Scripts/GetStockDataForAllDates.py3 -i Top200Composite -o StockData -d IBDdatabase.sqlite
