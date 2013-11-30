04e7e9ff046cc4f157a9eb5a0ab7a25f9cb11a8b
nick klosterman <nick_klosterman@yahoo.com>
2013-11-30 15:36:30 -0500

    new data, dl stock data in Sqlite script

--- SqliteGetAllUniqueTickersInIBDdatabase.sh
+++ SqliteGetAllUniqueTickersInIBDdatabase.sh
@@ -10,3 +10,21 @@ sqlite3 IBDdatabase.sqlite 'SELECT distinct(stockticker) from Top200Composite OR
 sort /tmp/tickers.txt -u > uniquetickers.txt
 
 echo "Unique tickers in uniquetickers.txt"
+
+rm /tmp/tickerurlfile.txt
+while read symbol
+do
+#echo "\"http://ichart.yahoo.com/table.csv?s=${symbol}&ignore=.csv\" -O ${symbol}.csv" >> /tmp/tickerurlfile.txt
+echo "http://ichart.yahoo.com/table.csv?s=${symbol}&ignore=.csv" >> /tmp/tickerurlfile.txt
+done < uniquetickers.txt
+
+cat /tmp/tickerurlfile.txt | parallel "wget {}"
+
+# Remove files of length/size 0
+find . -type f -size 0 -print0 | xargs -0 rm 
+
+
+
+#to determine how risky an investment would've been, do a query to find the min and max of each ticker and get the dates. see how close the dates are as well.
+
+#TFLN can be parallelized as well
