4695671fe691344000ad3e02d3a6efb5a6c21496
nick klosterman <nick_klosterman@yahoo.com>
2014-01-04 15:51:27 -0500

    superseded bu SqliteGetAllUniqueTickersInIBDDatabase.sh

--- OutputStockTickers.sh
+++ OutputStockTickers.sh
@@ -8,3 +8,5 @@ sqlite3 IBDdatabase.sqlite "select distinct stockticker from IBD8585" > /tmp/Tic
 sqlite3 IBDdatabase.sqlite "select distinct stockticker from IBD50" >> /tmp/TickerList.txt
 sqlite3 IBDdatabase.sqlite "select distinct stockticker from BC20" >> /tmp/TickerList.txt
 sqlite3 IBDdatabase.sqlite "select distinct stockticker from Top200Composite" >> /tmp/TickerList.txt
+
+#S----------------> superseded by SqliteGetAllUniqueTickersInIBDdatabase.sh
