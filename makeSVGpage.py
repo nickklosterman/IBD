f72cfa9252d92b02fea0305f3e51de0062074a0d
nickklosterman <nklosterman@lb.com>
2015-04-06 10:20:13 -0400

    added path to makeSVGPage

--- makeSVGpage.py
+++ makeSVGpage.py
@@ -2,23 +2,24 @@ import  SVGStock as SVGS
 
 import glob
 import sys
-if len(sys.argv)>1:
+if len(sys.argv)>2:
     if (sys.argv[1])!="":
         IBDList=sys.argv[1]
+        path=sys.argv[2]
 else:
-    print("error")
+    print("error, see usage")
     exit 
         
 filelist=sorted(glob.glob(IBDList+"_*.svg"))
 buffer="<html><body>"
 for item in filelist:
-    data=SVGS.SVGStock(item)
+    data=SVGS.SVGStock(item,path)
     #print(data.getSvgImgLink())
     #print(data.getYahoo2YrLogChartLink())
     #print(data.getYahooProfileLink())
     #print(data.getYahooKeyStatisticsLink())
 
-    buffer+=data.getSvgImgLink()+" "+data.getYahoo2YrLogChartLink()+" "+data.getYahooProfileLink()+" "+data.getYahooKeyStatisticsLink()
+    buffer+="<div>"+data.getSvgImgLink()+"</div><div>"+data.getYahoo2YrLogChartLink()+" "+data.getYahooProfileLink()+" "+data.getYahooKeyStatisticsLink()+"</div>"
 buffer+="</body></html>"
 filename=IBDList+".html"
 f=open(filename,'r+')
