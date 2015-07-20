f1761dbe73ba97481a4ce598d921a5c849bd71a6
Nick Klosterman <nick.klosterman@gmail.com>
2015-07-20 11:15:45 -0400

    data up to Jul 20; changed ExtractionAndInsertionRunner such that test is default case instead of write

--- Scripts/ExtractionAndInsertionRunner.sh
+++ Scripts/ExtractionAndInsertionRunner.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-if [[ $# -eq 1 ]]
-then 
+if [[ $# -eq 2 ]]
+then
+    if [[ ${2} == "write" ]]
+           then 
     date=`date +%F`
     path=${1}
     bash IBDPDFExtractor.sh ${path} | sort > /tmp/${date}_IBDDataSorted.txt #turning on any add'l echo statements will most likely break the EnterIBDDataFromFile script as the format will change
     python3 EnterIBDDataFromFile.py3 --inputfile=/tmp/${date}_IBDDataSorted.txt
-elif [[ $# -eq 2 ]]
+    else
+        echo "The first command will test if the data passes the checks and is clean and ready to be entered."
+        echo "Usage: ${0} path/to/files/"
+        echo "Usage: ${0} path/to/files/ write"
+        fi
+    
+elif [[ $# -eq 1 ]]
 then 
-    if [[ ${2} == "test" ]] 
-    then 
 	echo "Testing length of data to be entered."
-	echo "To write the results, run the script without the 'test' option."
+	echo "To write the results, run the script without the 'write' option."
+        echo "E.g. ${0} ${1} write"
 	date=`date +%F`
 	path=${1}
 	bash IBDPDFExtractor.sh ${path} | sort > /tmp/${date}_IBDDataSorted.txt #turning on any add'l echo statements will most likely break the EnterIBDDataFromFile script as the format will change
 	python3 EnterIBDDataFromFile.py3 --inputfile=/tmp/${date}_IBDDataSorted.txt --test
-    else
-	echo "Usage: ${0} path/to/files/ test #this will test for length correctness but not write out the data"
-    fi
 else
     
     echo "Usage: ${0} path/to/files/ test #this will test for length correctness but not write out the data"
