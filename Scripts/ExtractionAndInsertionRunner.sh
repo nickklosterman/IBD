0d842ebe25c92ba6daab47b6ef303a79caa38043
nickklosterman <nklosterman@c02gycv5dv7l.limited.brands.com>
2015-10-01 15:34:12 -0400

    update data to 10/1; ExtractionAndInsertionRunner modified to  use ~/Downloads as the default directory for input

--- Scripts/ExtractionAndInsertionRunner.sh
+++ Scripts/ExtractionAndInsertionRunner.sh
@@ -19,9 +19,24 @@ then
 	echo "To write the results, run the script without the 'write' option."
         echo "E.g. ${0} ${1} write"
 	date=`date +%F`
-	path=${1}
+    if [[ ${1} == "write" ]]
+    then
+	path="$HOME/Downloads/"
+	else 
+	    path=${1}
+	    fi
 	bash IBDPDFExtractor.sh ${path} | sort > /tmp/${date}_IBDDataSorted.txt #turning on any add'l echo statements will most likely break the EnterIBDDataFromFile script as the format will change
 	python3 EnterIBDDataFromFile.py3 --inputfile=/tmp/${date}_IBDDataSorted.txt --test
+elif [[ $# -eq 0 ]]  #default to checking the $HOME/Downloads directory 
+then 
+	echo "Testing length of data to be entered."
+	echo "To write the results, run the script without the 'write' option."
+        echo "E.g. ${0} ${1} write"
+	date=`date +%F`
+	path="$HOME/Downloads/"
+	bash IBDPDFExtractor.sh ${path} | sort > /tmp/${date}_IBDDataSorted.txt #turning on any add'l echo statements will most likely break the EnterIBDDataFromFile script as the format will change
+	python3 EnterIBDDataFromFile.py3 --inputfile=/tmp/${date}_IBDDataSorted.txt --test
+
 else
     
     echo "Usage: ${0} path/to/files/ test #this will test for length correctness but not write out the data"
