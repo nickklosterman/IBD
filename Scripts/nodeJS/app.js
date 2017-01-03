66d7844bb51a545bba42fd89c2aea9fb53a87fa6
nickklosterman <nick_klosterman@yahoo.com>
2017-01-02 23:46:38 -0500

    script w cookie

--- Scripts/nodeJS/app.js
+++ Scripts/nodeJS/app.js
@@ -5,34 +5,65 @@ var http = require('http'),
     postIDUrl="http://www.investors.com/ajax/static/postID:";
 
 function makeRequest(urlPiece){
-var options = {
-    host: 'research.investors.com',
-    path: '/Services/SiteAjaxService.asmx/'+urlPiece,
-    headers: {
-	'content-type': 'application/json; charset=utf-8',
-	cookie:cookie
-    }
-};
+    var options = {
+	host: 'research.investors.com',
+	path: '/Services/SiteAjaxService.asmx/'+urlPiece,
+	headers: {
+	    'content-type': 'application/json; charset=utf-8',
+	    cookie:cookie
+	}
+    };
 
-callback = function(response) {
-    var str = '';
+    callback = function(response) {
+	var str = '';
 
-    //another chunk of data has been received, so append it to `str`
-    response.on('data', function (chunk) {
-	str += chunk;
-    });
-
-    //the whole response has been received, so we just print it out here
-    response.on('end', function () {
-//	console.log(str);
-	var resp = JSON.parse(str);
-	resp.d.ETablesDataList.forEach(function(ele, indx,arr) {
-	    console.log(ele.Rank+" "+ele.Symbol);
+	//another chunk of data has been received, so append it to `str`
+	response.on('data', function (chunk) {
+	    str += chunk;
 	});
-    });
-}
 
-http.request(options, callback).end();
+	//the whole response has been received, so we just print it out here
+	response.on('end', function () {
+	    //	console.log(str);
+	    var resp = JSON.parse(str);
+	    resp.d.ETablesDataList.forEach(function(ele, indx,arr) {
+		console.log(ele.Rank+" "+ele.Symbol);
+	    });
+	});
+    }
+
+    http.request(options, callback).end();
+}
+function makeRequest2(obj){
+    var options = {
+	host: 'research.investors.com',
+	path: '/Services/SiteAjaxService.asmx/'+obj.urlPiece,
+	headers: {
+	    'content-type': 'application/json; charset=utf-8',
+	    cookie:cookie
+	}
+    };
+
+    callback = function(response) {
+	var str = '';
+
+	//another chunk of data has been received, so append it to `str`
+	response.on('data', function (chunk) {
+	    str += chunk;
+	});
+
+	//the whole response has been received, so we just print it out here
+	response.on('end', function () {
+	    console.log(obj.site);
+	    var resp = JSON.parse(str);
+	    resp.d.ETablesDataList.forEach(function(ele, indx,arr) {
+		console.log(ele.Rank+" "+ele.Symbol);
+	    });
+	    //console.log(JSON.stringify(resp.d.ETablesDataList));  //<-- this is the data that you'd write to the sql database
+	});
+    }
+
+    http.request(options, callback).end();
 }
 
 var sitesArr = [ 
@@ -42,20 +73,31 @@ var sitesArr = [
     'GetNewHigh?sortcolumn1="comprating"&sortOrder1="desc"&sortcolumn2="Symbol"&sortOrder2="ASC"', //d.ETablesDataList
     'GetIPOLeaders?sortcolumn1="comprating"&sortOrder1="desc"&sortcolumn2="Symbol"&sortOrder2="ASC"',//d.ETablesDataList
     'GetGlobalLeaders?sortcolumn1="rank"&sortOrder1="asc"&sortcolumn2="CompRating"&sortOrder2="ASC"',//d.ETablesDataList
-    
-    
     'GetWeeklyReview?sortcolumn1="indgrprank"&sortOrder1="asc"&sortcolumn2="symbol"&sortOrder2="ASC"'//d.ETablesDataList
 ],
     
     ibdDataTablesUrl={ url:"http://www.investors.com/ibd-data-tables/", host:"www.investors.com", path:"/ibd-data-tables"};
+
+
+sitesArr = [ 
+    {site:"IBD50",urlPiece:    'GetIBD50?sortcolumn1="ibd100rank"&sortOrder1="asc"&sortcolumn2=""&sortOrder2="ASC"'}, 
+    {site:"Sector Leaders",urlPiece:    'GetSectorLeaders?sortcolumn1="sectorrank"&sortOrder1="asc"&sortcolumn2="symbol"&sortOrder2="ASC"'}, 
+    {site:"Big Cap 20",urlPiece:    'GetBigCap20?sortcolumn1="ibd100rank"&sortOrder1="asc"&sortcolumn2=""&sortOrder2="ASC"'}, 
+    {site:"New High",urlPiece:    'GetNewHigh?sortcolumn1="comprating"&sortOrder1="desc"&sortcolumn2="Symbol"&sortOrder2="ASC"'}, 
+    {site:"IPO Leaders",urlPiece:    'GetIPOLeaders?sortcolumn1="comprating"&sortOrder1="desc"&sortcolumn2="Symbol"&sortOrder2="ASC"'},
+    {site:"Global Leaders",urlPiece:    'GetGlobalLeaders?sortcolumn1="rank"&sortOrder1="asc"&sortcolumn2="CompRating"&sortOrder2="ASC"'},
+
+    
+    {site:"Weekly Review",urlPiece:'GetWeeklyReview?sortcolumn1="indgrprank"&sortOrder1="asc"&sortcolumn2="symbol"&sortOrder2="ASC"'}//d.ETablesDataList
+];
 
 function getIBDDataTableUrls() {
     request(ibdDataTablesUrl.url,function(error,response,html) {
 	if (!error && response.statusCode == 200) {
 	    var $ = cheerio.load(html);
 	    $('.box-link').each(function(i,e){
-	//	    console.log(e.attribs.href);
-		    getPostID(e.attribs.href);
+		//	    console.log(e.attribs.href);
+		getPostID(e.attribs.href);
 	    });
 	}
     });
@@ -67,7 +109,7 @@ function getPostID(url) {
 	if (!error && response.statusCode == 200) {
 	    var $ = cheerio.load(html);
 	    console.log(url +" -=- "+ ($("link[rel='shortlink']")[0]).attribs.href.split("p=")[1] );
-//	    getPost($("link[rel='shortlink']")[0]).attribs.href.split("p=")[1] ); //need to pass in some stuff to set the offset for the correct data to be grabbed from the lists
+	    //	    getPost($("link[rel='shortlink']")[0]).attribs.href.split("p=")[1] ); //need to pass in some stuff to set the offset for the correct data to be grabbed from the lists
 	}
     });
 }
@@ -90,7 +132,8 @@ function getPost(id) {
 
 function main(cookie) {
     sitesArr.forEach(function(ele,idx,arr) {
-	makeRequest(ele);	
+	//	makeRequest(ele);
+		makeRequest2(ele);	
     });
 
     //      getIBDDataTableUrls();
@@ -100,43 +143,43 @@ function main(cookie) {
     //    getPost(294350);
     
     /*
-    http://www.investors.com/data-tables/company-earnings-reports-august-9-2016/ -=- 294462
-    http://www.investors.com/data-tables/ipos-august-10-2016/ -=- 295498
-    http://www.investors.com/data-tables/psychological-indicators-volume-august-9-2016/ -=- 294486
-    http://www.investors.com/data-tables/expanded-52-week-highs-and-lows-august-10-2016/ -=- 295606
-    http://www.investors.com/data-tables/ibd-smart-nyse-nasdaq-tables-august-10-2016/ -=- 295611
-    http://www.investors.com/data-tables/dividends-august-10-2016/ -=- 295502
-    http://www.investors.com/data-tables/expanded-mutual-fund-tables-august-9-2016/ -=- 294890
-    http://www.investors.com/data-tables/mutual-fund-charts-august-9-2016/ -=- 294521
-    http://www.investors.com/data-tables/top-sector-etfs-august-10-2016/ -=- 295540
-    http://www.investors.com/data-tables/ibd-smallmid-cap-leaders-index-august-10-2016/ -=- 295677
-    http://www.investors.com/data-tables/top-200-composite-stocks-august-10-2016/ -=- 295623
-    http://www.investors.com/data-tables/industry-sub-group-rankings-august-9-2016/ -=- 294451
-    http://www.investors.com/data-tables/bottom-200-composite-stocks-august-10-2016/ -=- 295585
-    http://www.investors.com/data-tables/ibd-tech-leaders-august-10-2016/ -=- 295682
-    http://www.investors.com/data-tables/reit-leaders-august-10-2016/ -=- 295515
-    http://www.investors.com/data-tables/dividend-leaders-august-10-2016/ -=- 295508
-    http://www.investors.com/data-tables/sector-rotation-etfs-august-9-2016/ -=- 294447
-    http://www.investors.com/data-tables/futures-tables-august-10-2016/ -=- 295663
-    http://www.investors.com/data-tables/timesaver-table-august-10-2016/ -=- 295619
-    http://www.investors.com/data-tables/options-august-9-2016/ -=- 294516
-    http://www.investors.com/data-tables/top-fidelity-sector-funds-august-9-2016/ -=- 294499
-    http://www.investors.com/data-tables/etf-tables-august-10-2016/ -=- 295524
-    http://www.investors.com/data-tables/stocks-out-of-bases-august-10-2016/ -=- 295555
-    http://www.investors.com/data-tables/futures-charts-august-9-2016/ -=- 294481
-    http://www.investors.com/data-tables/etf-winners-august-10-2016/ -=- 295536
-    http://www.investors.com/data-tables/groups-with-highest-of-stocks-at-new-high-august-10-2016/ -=- 295544
-    http://www.investors.com/data-tables/reits-august-10-2016/ -=- 295615
-    http://www.investors.com/data-tables/spot-prices-august-10-2016/ -=- 295550
-    http://www.investors.com/data-tables/etf-losers-august-10-2016/ -=- 295531
-    http://www.investors.com/data-tables/interest-rates-august-9-2016/ -=- 294474
-    http://www.investors.com/data-tables/find-your-groups-sector/ -=- 220397
-    http://www.investors.com/data-tables/new-high-list-august-10-2016/ -=- 295601
-    http://www.investors.com/data-tables/utility-leaders-august-10-2016/ -=- 295519
-    http://www.investors.com/data-tables/top-ranked-low-priced-stocks-august-10-2016/ -=- 295627
-    http://www.investors.com/data-tables/nyse-nasdaq-most-active-and-most-up-august-10-2016/ -=- 295593
-    http://www.investors.com/data-tables/ibd-largemid-cap-leaders-index-august-10-2016/ -=- 295669
-    http://www.investors.com/data-tables/preferred-stocks-august-9-2016/ -=- 294490
+      http://www.investors.com/data-tables/company-earnings-reports-august-9-2016/ -=- 294462
+      http://www.investors.com/data-tables/ipos-august-10-2016/ -=- 295498
+      http://www.investors.com/data-tables/psychological-indicators-volume-august-9-2016/ -=- 294486
+      http://www.investors.com/data-tables/expanded-52-week-highs-and-lows-august-10-2016/ -=- 295606
+      http://www.investors.com/data-tables/ibd-smart-nyse-nasdaq-tables-august-10-2016/ -=- 295611
+      http://www.investors.com/data-tables/dividends-august-10-2016/ -=- 295502
+      http://www.investors.com/data-tables/expanded-mutual-fund-tables-august-9-2016/ -=- 294890
+      http://www.investors.com/data-tables/mutual-fund-charts-august-9-2016/ -=- 294521
+      http://www.investors.com/data-tables/top-sector-etfs-august-10-2016/ -=- 295540
+      http://www.investors.com/data-tables/ibd-smallmid-cap-leaders-index-august-10-2016/ -=- 295677
+      http://www.investors.com/data-tables/top-200-composite-stocks-august-10-2016/ -=- 295623
+      http://www.investors.com/data-tables/industry-sub-group-rankings-august-9-2016/ -=- 294451
+      http://www.investors.com/data-tables/bottom-200-composite-stocks-august-10-2016/ -=- 295585
+      http://www.investors.com/data-tables/ibd-tech-leaders-august-10-2016/ -=- 295682
+      http://www.investors.com/data-tables/reit-leaders-august-10-2016/ -=- 295515
+      http://www.investors.com/data-tables/dividend-leaders-august-10-2016/ -=- 295508
+      http://www.investors.com/data-tables/sector-rotation-etfs-august-9-2016/ -=- 294447
+      http://www.investors.com/data-tables/futures-tables-august-10-2016/ -=- 295663
+      http://www.investors.com/data-tables/timesaver-table-august-10-2016/ -=- 295619
+      http://www.investors.com/data-tables/options-august-9-2016/ -=- 294516
+      http://www.investors.com/data-tables/top-fidelity-sector-funds-august-9-2016/ -=- 294499
+      http://www.investors.com/data-tables/etf-tables-august-10-2016/ -=- 295524
+      http://www.investors.com/data-tables/stocks-out-of-bases-august-10-2016/ -=- 295555
+      http://www.investors.com/data-tables/futures-charts-august-9-2016/ -=- 294481
+      http://www.investors.com/data-tables/etf-winners-august-10-2016/ -=- 295536
+      http://www.investors.com/data-tables/groups-with-highest-of-stocks-at-new-high-august-10-2016/ -=- 295544
+      http://www.investors.com/data-tables/reits-august-10-2016/ -=- 295615
+      http://www.investors.com/data-tables/spot-prices-august-10-2016/ -=- 295550
+      http://www.investors.com/data-tables/etf-losers-august-10-2016/ -=- 295531
+      http://www.investors.com/data-tables/interest-rates-august-9-2016/ -=- 294474
+      http://www.investors.com/data-tables/find-your-groups-sector/ -=- 220397
+      http://www.investors.com/data-tables/new-high-list-august-10-2016/ -=- 295601
+      http://www.investors.com/data-tables/utility-leaders-august-10-2016/ -=- 295519
+      http://www.investors.com/data-tables/top-ranked-low-priced-stocks-august-10-2016/ -=- 295627
+      http://www.investors.com/data-tables/nyse-nasdaq-most-active-and-most-up-august-10-2016/ -=- 295593
+      http://www.investors.com/data-tables/ibd-largemid-cap-leaders-index-august-10-2016/ -=- 295669
+      http://www.investors.com/data-tables/preferred-stocks-august-9-2016/ -=- 294490
     */
 }
 
@@ -146,7 +189,7 @@ if (process.argv[2]) {
     console.log("no cookie provided");
 }
 
-//main(cookie)
+main(cookie)
 var now=new Date();
 console.log(now);
 var dateForInsertion = now.getUTCFullYear()+"-"+(now.getUTCMonth()+1)+"-"+now.getUTCDate();
@@ -156,6 +199,6 @@ console.log(dateForInsertion);
 
 
 /*TODO:
-know which list to write the data to,
-disallow writing if there is no cookie as this won't write a full list.
+  know which list to write the data to,
+  disallow writing if there is no cookie as this won't write a full list.
 */
